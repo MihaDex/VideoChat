c293c6da75f67d90d2aa02b84bd46af0b53b5705
Mikhail Dementev <dex-max89@mail.ru>
2019-03-28 22:24:01 +0300

    Sdelan tekstovii chat

--- config/socket.js
+++ config/socket.js
@@ -21,17 +21,21 @@ module.exports = function(io){
     secret: process.env.JWT_SECRET,
     timeout: 15000 // 15 seconds to send the authentication message
   })).on('authenticated', function(socket) {
-    // //this socket is authenticated, we are good to handle more events from it.
-    // console.log('hello! ' + socket.decoded_token.name);
-    var users = [];
-    for (var key in io.sockets.sockets) {
-        console.log(io.sockets.sockets[key].decoded_token.name);
-        // if (io.sockets.sockets[key].username === data.nick) {
-        //   io.sockets.sockets[key].emit("msg", data.msg);
-        users.push({name: io.sockets.sockets[key].decoded_token.name, msg: ""});
-        }
-    //     
-    //   }
-      io.emit('users', users);
-  });
+
+        var name = socket.decoded_token.name;
+        var users = [];
+        var messages = [];
+        //messages.push({name: name, msg: "Пользователь присоединился к чату"});
+
+        socket.on('message', function(msg){
+            messages.push({name: name, msg: msg});
+            io.sockets.emit('messageToClients',messages);
+        });
+
+        for (var key in io.sockets.sockets) {
+            if(io.sockets.sockets[key].decoded_token.name){
+            users.push({name: io.sockets.sockets[key].decoded_token.name, msg: ""});
+        }}
+        io.emit('users', users);
+    });
 }
--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -11,18 +11,46 @@
             $scope.auth=false;
         }
         
+        $scope.error='';
+        $scope.inputMsg='';
+        $scope.call = function(){
+            
+        }
+
         var socket = io();
 
             socket.on('connect', function () {
                 socket
                   .emit('authenticate', {token: authentication.getToken()}) //send the jwt
                   .on('authenticated', function () {
+
                     socket.on('users', function(data){
-                        console.log(data);
                         $scope.$apply(function() {
                             $scope.users = data;
                         });
                     })
+                    // socket.on('newUser',function(name){
+                    //     $scope.$apply(function(){
+                    //         $scope.messages.push({name: name, msg: "Пользователь присоединился к чату"});
+                    //     })
+                    // })
+                    // socket.on('userName',function(name){
+                    //     console.log('hello'+name);
+                    // })
+                    socket.on('messageToClients', function(messages){
+                        
+                        $scope.$apply(function(){
+                            $scope.messages = messages;
+                        })
+                    })
+                    
+                    $scope.send = function(){
+                      $scope.errors='';
+                      if($scope.inputMsg){
+                        socket.emit('message',$scope.inputMsg)
+                      }
+                    }
+
                   })
                   .on('unauthorized', function(msg) {
                     console.log("unauthorized: " + JSON.stringify(msg.data));
