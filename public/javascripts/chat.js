9cbccb4901e2109911978cdf71ffc0bdf14ce434
Mikhail Dementev <dex-max89@mail.ru>
2019-04-01 22:08:02 +0300

    video test

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,11 @@
 (function(){
-    
+    var localVideo = document.querySelector('#localVideo'); 
+    var remoteVideo = document.querySelector('#remoteVideo'); 
+    var yourConn; 
+    var stream;
+    var callName;
+
+
     angular
         .module('VideoChat')
         .controller('chatCtrl', chatCtrl)
@@ -10,11 +16,52 @@
         }else{
             $scope.auth=false;
         }
-        
         $scope.error='';
         $scope.inputMsg='';
-        $scope.call = function(){
-            
+        console.log(navigator);
+        $scope.openCall = function(name){
+                callName = name;
+               // Prefer camera resolution nearest to 1280x720.
+                var constraints = { audio: true, video: { width: 1280, height: 720 } }; 
+
+                navigator.mediaDevices.getUserMedia(constraints)
+                .then(function(mediaStream) {
+                var video = document.querySelector('video');
+                video.srcObject = mediaStream;
+                video.onloadedmetadata = function(e) {
+                    video.play();
+                };
+                })
+                .catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
+        }
+        $scope.callBtn = function () { 
+            var callToUsername = callName;
+             
+            if (callToUsername.length > 0) { 
+             
+               connectedUser = callToUsername;
+                 
+               // create an offer 
+               yourConn.createOffer(function (offer) { 
+                  send({ 
+                     type: "offer", 
+                     offer: offer 
+                  }); 
+                     
+                  yourConn.setLocalDescription(offer); 
+               }, function (error) { 
+                  alert("Error when creating an offer"); 
+               });
+                 
+            } 
+         };
+        $scope.closeBtn = function(){
+            connectedUser = null; 
+            remoteVideo.src = null; 
+             
+            yourConn.close(); 
+            yourConn.onicecandidate = null; 
+            yourConn.onaddstream = null; 
         }
 
         var socket = io();
