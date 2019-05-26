abe892b62ef930f59227a31408b82868a939a6bc
Mihail <dex-max89@mail.ru>
2019-05-26 18:10:16 +0300

    Video

--- config/socket.js
+++ config/socket.js
@@ -1,5 +1,8 @@
 var socketioJwt   = require("socketio-jwt");
-
+var users = [];
+var messages = [];
+var getUsers;
+var room = '';
 module.exports = function(io){
     // io.use(socketioJwt.authorize({
     //     secret: process.env.JWT_SECRET,
@@ -22,20 +25,201 @@ module.exports = function(io){
     timeout: 15000 // 15 seconds to send the authentication message
   })).on('authenticated', function(socket) {
 
-        var name = socket.decoded_token.name;
-        var users = [];
-        var messages = [];
-        //messages.push({name: name, msg: "Пользователь присоединился к чату"});
+    getUsers = function(room){
+        users = [];
+        for (var key in io.sockets.adapter.rooms[room].sockets) {
+            if(io.sockets.sockets[key].decoded_token.name){
+            users.push({name: io.sockets.sockets[key].decoded_token.name, id:io.sockets.sockets[key].id, msg: ""});
+        }}
+    }
 
-        socket.on('message', function(msg){
-            messages.push({name: name, msg: msg});
-            io.sockets.emit('messageToClients',messages);
+    // if(users.length == 0){
+    //     users.push({id: socket.id, token: socket.decoded_token})
+    // }
+    // users.forEach(function(v, i , users){
+    //     if(users[i].id == socket.id && users[i].token._id == socket.decoded_token._id ){
+    //         console.log('da');
+    //     } else{
+    //         console.log('net');
+    //         users.push({id: socket.id, token: socket.decoded_token})
+    //     }
+    // });
+    socket.join('all');
+    getUsers('all');
+    console.log(users);
+        // console.log(socket.id);
+        // if(key.id  !=  socket.id){
+        //     console.log('test');
+        //     users.push({id: socket.id,token: socket.decoded_token});
+        // } 
+    
+        // console.log(users);
+        //messages.push({name: name, msg: "Пользователь присоединился к чату"});
+        // for (var key in io.sockets.sockets) {
+        //     console.log('test');    
+        //     // users.push({name: io.sockets.sockets[key].decoded_token.name, msg: "", id: socket.id});
+        //     }
+
+        function sendTo(room,obj){
+            console.log('typey: ',obj.type);
+            console.log(obj);
+            socket.broadcast.to(room).emit('send',obj);
+            // socket.broadcast.emit('send',obj);
+
+        }
+        socket.on('roomJoin', function(msg){
+            room = msg.name;
+            socket.join(room);
+            io.sockets.sockets[room].join(room);
+            
+            getUsers(room);
+            io.to(room).emit('users', users);
+
+            io.to(room).emit('openCall', 'test');
+
+        })
+        socket.on('leaveall', function(){
+            socket.leave('all');
+            getUsers('all');
+            io.to('all').emit('users', users);
+        })
+        socket.on('serverSend', function(data){
+            
+            console.log(data);
+            switch (data.type) {
+                case "offer": 
+                
+                sendTo(room,{ 
+                    type: "offer", 
+                    offer: data.offer
+                }); 
+            
+                
+                break;
+                
+            case "answer": 
+               
+                sendTo(room,{ 
+                    type: "answer", 
+                    answer: data.answer 
+                }); 
+                 
+                    
+                break; 
+                
+            case "candidate": 
+               
+                sendTo(room,{ 
+                    type: "candidate", 
+                    candidate: data.candidate 
+                }); 
+                
+                    
+                break;
+                
+            case "leave": 
+                
+                sendTo(room,{ 
+                    type: "leave" 
+                }); 
+                
+                    
+                break;
+                
+            default: 
+                sendTo(room,{ 
+                type: "error", 
+                message: "Command not found: " + data.type 
+                }); 
+                    
+                break; 
+            }
         });
 
-        for (var key in io.sockets.sockets) {
-            if(io.sockets.sockets[key].decoded_token.name){
-            users.push({name: io.sockets.sockets[key].decoded_token.name, msg: ""});
-        }}
+        socket.on('message', function(msg){
+            messages.push({name: socket.decoded_token.name, msg: msg});
+            io.sockets.emit('messageToClients',messages);
+        });
+        socket.on('serverSend1', function(data){
+            switch (data.type) {
+                case "offer": 
+                //for ex. UserA wants to call UserB 
+                console.log("Sending offer to: ", data.name);
+                    
+                //if UserB exists then send him offer details 
+                var conn = users[data.name]; 
+                    
+                if(conn != null) { 
+                //setting that UserA connected with UserB 
+                connection.otherName = data.name; 
+                        
+                sendTo(conn, { 
+                    type: "offer", 
+                    offer: data.offer, 
+                    name: connection.name 
+                }); 
+                }
+				
+                break;
+				
+            case "answer": 
+                console.log("Sending answer to: ", data.name); 
+                //for ex. UserB answers UserA 
+                var conn = users[data.name]; 
+                    
+                if(conn != null) { 
+                connection.otherName = data.name; 
+                sendTo(conn, { 
+                    type: "answer", 
+                    answer: data.answer 
+                }); 
+                } 
+                    
+                break; 
+				
+            case "candidate": 
+                console.log("Sending candidate to:",data.name); 
+                var conn = users[data.name];
+                    
+                if(conn != null) { 
+                sendTo(conn, { 
+                    type: "candidate", 
+                    candidate: data.candidate 
+                }); 
+                } 
+                    
+                break;
+				
+            case "leave": 
+                console.log("Disconnecting from", data.name); 
+                var conn = users[data.name]; 
+                conn.otherName = null; 
+                    
+                //notify the other user so he can disconnect his peer connection 
+                if(conn != null) {
+                sendTo(conn, { 
+                    type: "leave" 
+                }); 
+                }
+                    
+                break;
+				
+            default: 
+                sendTo(connection, { 
+                type: "error", 
+                message: "Command not found: " + data.type 
+                }); 
+                    
+                break; 
+            }
+        });
+
+         
+
         io.emit('users', users);
-    });
+
+        socket.on('disconnect', function() {
+            users.splice(users.indexOf(socket), 1);
+        });
+    })
 }
--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,10 +1,25 @@
 (function(){
     var localVideo = document.querySelector('#localVideo'); 
     var remoteVideo = document.querySelector('#remoteVideo'); 
-    var yourConn; 
+    var pc; 
     var stream;
     var callName;
-
+    var PeerConnection = window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+    var SessionDescription = window.mozRTCSessionDescription || window.RTCSessionDescription;
+    var IceCandidate = window.mozRTCIceCandidate || window.RTCIceCandidate;
+    var server = {
+        iceServers: [
+            {url: "stun:23.21.150.121"},
+            {url: "stun:stun.l.google.com:19302"},
+            {url: "turn:numb.viagenie.ca", credential: "your password goes here", username: "example@example.com"}
+        ]
+    };
+    var options = {
+        optional: [
+            {DtlsSrtpKeyAgreement: true}, // требуется для соединения между Chrome и Firefox
+            {RtpDataChannels: true} // требуется в Firefox для использования DataChannels API
+        ]
+    }
 
     angular
         .module('VideoChat')
@@ -18,53 +33,10 @@
         }
         $scope.error='';
         $scope.inputMsg='';
-        console.log(navigator);
-        $scope.openCall = function(name){
-                callName = name;
-               // Prefer camera resolution nearest to 1280x720.
-                var constraints = { audio: true, video: { width: 1280, height: 720 } }; 
-
-                navigator.mediaDevices.getUserMedia(constraints)
-                .then(function(mediaStream) {
-                var video = document.querySelector('video');
-                video.srcObject = mediaStream;
-                video.onloadedmetadata = function(e) {
-                    video.play();
-                };
-                })
-                .catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
-        }
-        $scope.callBtn = function () { 
-            var callToUsername = callName;
-             
-            if (callToUsername.length > 0) { 
-             
-               connectedUser = callToUsername;
-                 
-               // create an offer 
-               yourConn.createOffer(function (offer) { 
-                  send({ 
-                     type: "offer", 
-                     offer: offer 
-                  }); 
-                     
-                  yourConn.setLocalDescription(offer); 
-               }, function (error) { 
-                  alert("Error when creating an offer"); 
-               });
-                 
-            } 
-         };
-        $scope.closeBtn = function(){
-            connectedUser = null; 
-            remoteVideo.src = null; 
-             
-            yourConn.close(); 
-            yourConn.onicecandidate = null; 
-            yourConn.onaddstream = null; 
-        }
-
+        var serverSend;
+        var roomJoin;
         var socket = io();
+       
 
             socket.on('connect', function () {
                 socket
@@ -72,25 +44,92 @@
                   .on('authenticated', function () {
 
                     socket.on('users', function(data){
+                        
+                        data = data.filter(element => {
+                            if(element.id != socket.id){
+                                return true;
+                            }
+                        });
                         $scope.$apply(function() {
+                            
                             $scope.users = data;
                         });
                     })
-                    // socket.on('newUser',function(name){
-                    //     $scope.$apply(function(){
-                    //         $scope.messages.push({name: name, msg: "Пользователь присоединился к чату"});
-                    //     })
-                    // })
-                    // socket.on('userName',function(name){
-                    //     console.log('hello'+name);
-                    // })
+
                     socket.on('messageToClients', function(messages){
-                        
                         $scope.$apply(function(){
                             $scope.messages = messages;
                         })
                     })
-                    
+                    socket.on('openCall', function(msg){
+                        // console.log(msg);
+                        // socket.emit('pm','answer');
+                        // var constraints = { audio: true, video: { width: 1280, height: 720 } }; 
+
+                        socket.emit('leaveall');
+
+                        $("#exampleModalCenter").modal('show');
+                            
+                        var constraints = {video: true }; 
+                        
+                        // navigator.mediaDevices.getUserMedia(constraints)
+                        navigator.mediaDevices.getDisplayMedia(constraints)
+                        .then(function(mediaStream) {
+                        var video = document.querySelector('video');
+                        video.srcObject = mediaStream;
+                        video.onloadedmetadata = function(e) {
+                            video.play();
+                        };
+
+                       
+                        pc = new PeerConnection(server,options);
+                        pc.addStream(mediaStream);
+                        pc.onaddstream = function (e) { 
+                            remoteVideo.srcObject = e.stream; 
+                        };
+                        pc.onicecandidate = function (event) { 
+                            if (event.candidate) { 
+                                serverSend({ 
+                                type: "candidate", 
+                                candidate: event.candidate 
+                            }); 
+                            } 
+                        };
+                            
+                        
+
+                        })
+                        .catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
+                    })
+
+                    socket.on('send',function(msg){
+                        console.log(msg);
+                        switch(msg.type){
+                            case "candidate":
+                                handleCandidate(msg.candidate);
+                                break;
+                            case "answer":
+                                handleAnswer(msg.answer);
+                                break;
+                            case "offer":
+                            handleOffer(msg.offer);
+                            break;
+                        }
+                    })
+
+                    roomJoin = function(obj){
+                        if(obj){
+                            socket.emit('roomJoin', {'name': obj.id});
+                        }
+                    };
+
+                    serverSend = function(msg){
+                        // if(callName){
+                        //     msg.name = callName;
+                        // }
+                        console.log(msg);
+                        socket.emit('serverSend',msg);
+                    };
                     $scope.send = function(){
                       $scope.errors='';
                       if($scope.inputMsg){
@@ -104,5 +143,105 @@
                     throw new Error(msg.data.type);
                   })
               });
+
+        $scope.openCall = function(id){
+                // callName = name;
+               // Prefer camera resolution nearest to 1280x720.
+
+                roomJoin({
+                    id: id,
+                });
+
+                // var constraints = { audio: true, video: { width: 1280, height: 720 } }; 
+                var constraints = {video: true }; 
+                
+                // navigator.mediaDevices.getUserMedia(constraints)
+                navigator.mediaDevices.getDisplayMedia(constraints)
+                .then(function(mediaStream) {
+                var video = document.querySelector('video');
+                video.srcObject = mediaStream;
+                video.onloadedmetadata = function(e) {
+                    video.play();
+                };
+
+                pc = new PeerConnection(server,options);
+                pc.addStream(mediaStream);
+                pc.onaddstream = function (e) { 
+                    remoteVideo.srcObject = e.stream; 
+                 };
+                pc.onicecandidate = function (event) { 
+                    if (event.candidate) { 
+                        serverSend({ 
+                          type: "candidate", 
+                          candidate: event.candidate 
+                       }); 
+                    } 
+                 };
+
+                })
+                .catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
+        }
+        $scope.callBtn = function () { 
+            console.log('rew');
+            // var callToUsername = callName;
+            // if (callToUsername.length > 0) { 
+             
+            //    connectedUser = callToUsername;
+                 
+            //    // create an offer 
+               pc.createOffer(function (offer) { 
+                serverSend({ 
+                     type: "offer", 
+                     offer: offer 
+                  }); 
+                     
+                  pc.setLocalDescription(offer); 
+               }, function (error) { 
+                  alert("Error when creating an offer"); 
+               });
+                 
+            
+         };
+        $scope.closeBtn = function(){
+            connectedUser = null; 
+            remoteVideo.src = null; 
+            serverSend({ 
+                type: "leave" 
+             });  
+            pc.close(); 
+            pc.onicecandidate = null; 
+            pc.onaddstream = null; 
+        }
+
+        //when somebody sends us an offer 
+        function handleOffer(offer) { 
+            
+            pc.setRemoteDescription(new SessionDescription(offer));
+            
+            //create an answer to an offer 
+            pc.createAnswer(function (answer) { 
+                pc.setLocalDescription(answer); 
+                
+            serverSend({ 
+                type: "answer", 
+                answer: answer 
+            }); 
+                
+            }, function (error) { 
+            alert("Error when creating an answer"); 
+            }); 
+        };
+        
+        //when we got an answer from a remote user
+        function handleAnswer(answer) { 
+            pc.setRemoteDescription(new SessionDescription(answer)); 
+        };
+        
+        //when we got an ice candidate from a remote user 
+        function handleCandidate(candidate) { 
+            pc.addIceCandidate(new IceCandidate(candidate)); 
+        };
+
+        
     }
 })();
